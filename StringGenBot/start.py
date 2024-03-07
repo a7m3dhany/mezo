0a07f57fd378f02230124a8fa7b033e58d216b8b
DEV_DARK <157925611+a7m3dhany@users.noreply.github.com>
2024-03-07 19:06:18 +0200

    Add files via upload

---
+++ StringGenBot/start.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""۞¦ اهلا بـك عزيـزي  {msg.from_user.mention}
+۞¦ فـي بـوت اسـتـخـراج الـجـلـسـات
+۞¦ يمكنك استخراج الجلسات الـتالية
+۞¦ بايروجرام للحسابات & بايروجرام للبوتات
+۞¦ بـايـروجـرام مـيوزك احـدث إصـدار 
+۞¦ تيرمـكـس للحسابات & تيرمـكـس للبوتات
+
+۞¦ بواسطـة : [ ꪑꪖɀꫀꪀ | ᦔꫀꪜ ...𐇮](tg://user?id=5951674553) √""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="📥 ⍆ اضغط لبدا استخراج كود ⍅ 📥", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("𝑆𝑂ᦔꫀꪜ ꪑꪖɀꫀꪀ", url="https://t.me/Ve_G4"),
+                    InlineKeyboardButton("𝙳𝙴𝚅 𝙳𝙰𝚁𝙺", user_id=5951674553)
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
